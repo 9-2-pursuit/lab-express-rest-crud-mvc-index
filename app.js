046180179354113a94a44760bd4f1d8b0f26444d
Bo Latt <bolattny@gmail.com>
2022-12-18 12:09:08 -0500

    done with bonus

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const specialEvents = require("./controllers/special-events.controller");
+const locations = require("./controllers/locations.controller");
+const machines = require("./controllers/machines.controller");
+const people = require("./controllers/persons.controller");
+const plans = require("./controllers/plans.controller");
+const peopleByLocation = require("./controllers/peopleByLocation.controller");
+
+const app = express();
+
+app.use("/locations", locations);
+app.use("/machines", machines);
+app.use("/persons", people);
+app.use("/plans", plans);
+app.use("/special-events", specialEvents);
+app.use("/locations/people", peopleByLocation);
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send("Sorry, no page found!");
+});
+
+module.exports = app;
