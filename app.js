f6ee8623f70faaf6ed720a6d73823cd5607b52a5
SasukeDaTony <tonytaylor@pursuit.org>
2022-12-18 12:47:07 -0500

    models completed - controllers almost done

--- app.js
+++ app.js
@@ -9,5 +9,20 @@ app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
 
+//ErrorHandle
+// app.get("*", (req, res) => {
+//   res.json({ error: "Sorry, no page found!" });
+// });
+
+// index Route Consts
+const locationsController = require("./controllers/locations.controller.js");
+app.use("/locations", locationsController);
+
+const machinesController = require("./controllers/machines.controller.js");
+app.use("/machines", machinesController);
+
+const specialEventsController = require("./controllers/special-events.controller.js");
+app.use("/special-events", specialEventsController);
+
 //Export
 module.exports = app;
