ec0659d602fc95fb9b0caf71aea6d2ab93f6b8ba
i-guerrero <isaacguerrero@pursuit.org>
2022-12-18 12:08:28 -0500

    Add work around for locations/people 23 Broadway location

--- controllers/locations.controller.js
+++ controllers/locations.controller.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const locations = express.Router();
 const locationsArray = require("../models/location.model.js");
+const personsArray = require("../models/person.model");
 
 locations.get("/", (req, res) => {
+  res.json(locationsArray);
+});
+
+locations.get("/people", (req, res) => {
+  const locationsArrayCopy = [...locationsArray];
+  locationsArrayCopy.map((loc) => {
+    return (loc.people = personsArray.filter((person) => {
+      // || work around for 23 Broadway
+      return (
+        person.mainLocation === loc.zip ||
+        (person.mainLocation === "10011" && loc.zip === "11011")
+      );
+    }));
+  });
   res.json(locationsArray);
 });
 
