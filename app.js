bf7f2c8ffc3f8f8d5a0b1c41f4cafa9b4cb2f7c9
i-guerrero <isaacguerrero@pursuit.org>
2022-12-18 11:32:03 -0500

    Set up locations and machines controllers

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // DEPENDENCIES
 const express = require("express");
+const locationsController = require("./controllers/locations.controller");
+const machinesController = require("./controllers/machines.controller");
 
 // CONFIGURATION
 const app = express();
@@ -8,6 +10,10 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
+
+app.use("/locations", locationsController);
+
+app.use("/machines", machinesController);
 
 // 404
 app.get("*", (req, res) => {
