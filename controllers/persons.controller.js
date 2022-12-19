ce6be69a884b1a8e16487a52f97b2e2de00e447c
Karyn Huston <huston.karyn@gmail.com>
2022-12-18 20:44:28 -0500

    all tests passing

--- controllers/persons.controller.js
+++ controllers/persons.controller.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const people = express.Router();
-const peopleArr = require("../models/person.model");
+const peopleArr = require("../models/person.model.js");
 
 people.get("/", (req, res) => {
   res.json(peopleArr);
