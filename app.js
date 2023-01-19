eba77e5a2e12cd47245eaaeeafefb0a1cd409ebf
cjs2408 <carlossuero@pursuit.org>
2023-01-18 19:19:41 -0500

    Submission 1

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express")
+
+const locations = require("./controllers/locations.controller.js")
+const persons = require("./controllers/persons.controller")
+const machines = require("./controllers/machines.controller")
+const plans = require("./controllers/plans.controller")
+const specialEvents = require("./controllers/special-events.controller")
+
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello, world!"));
+
+app.get("/locations", locations)
+app.get("/persons", persons)
+app.get("/machines", machines)
+app.get("/plans", plans)
+app.get("/special-events", specialEvents)
+
+app.get((req, res) => {
+  res.send("Sorry, no page found!")
+})
+
+module.exports = app;
---
+++ controllers/locations.controller.js
@@ -0,0 +1,9 @@
+
+
+const locationsData = require("../models/location.model");
+
+const locations = (req, res) => {
+  res.send(locationsData);
+}
+
+module.exports = locations;
